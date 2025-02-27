54790d647dfb6341854ca282ff9fab3f33482f8f
rayswag0825 <tl3337@columbia.edu>
2025-02-27 03:27:39 -0500

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def FizzBuzz(start, finish):
+    outlist = []
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            outlist.append("fizzbuzz")
+        elif i % 3 == 0:
+            outlist.append("fizz")
+        elif i % 5 == 0:
+            outlist.append("buzz")
+        else:
+            outlist.append(i)
+    return outlist
+
+# Test the function
+print(FizzBuzz(1, 15))
